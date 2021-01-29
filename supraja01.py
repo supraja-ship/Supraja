8e7acf5ccb8d7e63f2a0f4b28ab68c5a304f7829
supraja-ship <mendasupraja@gmail.com>
2021-01-29 15:15:04 +0530

    first commit

---
+++ supraja01.py
@@ -0,0 +1,5 @@
+a=int(input('enter'))
+b=int(input('enter'))
+print("addition of value is :-",a+b)
+
+      
---
+++ supraja02.py
@@ -0,0 +1,11 @@
+a=int(input("enter"))
+b=int(input("enter"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+      
+      
